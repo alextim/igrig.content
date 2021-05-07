a1f3b0082ae3b03017a7d64420ca3f8f0d58b323
Alex Tim <buildmnt@gmail.com>
2021-05-07 17:08:30 +0300

    content update

--- git-add-commit-push.bat
+++ git-add-commit-push.bat
@@ -1,5 +1,14 @@
+@ECHO OFF
+SETLOCAL ENABLEEXTENSIONS
+SET commit_comment=
+SET /p commit_comment=Input your comment for commit:
+IF NOT DEFINED commit_comment (
+  SET commit_comment=content update
+  ECHO Default comment will be used for this commit
+)
+
 git status
 git add .
 git status
-git commit -m "content update"
+git commit -m "%commit_comment%"
 git push
